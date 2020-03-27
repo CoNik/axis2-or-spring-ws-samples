8cc80559c898bc4eac80016e157ecf2490f958c8
CoNik <13494900+CoNik@users.noreply.github.com>
2020-03-26 20:33:33 -0500

    Add files via upload

---
+++ ws-sample-consumer/src/test/java/org/java/ws/sample/WsSampleConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.ws.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSampleConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
